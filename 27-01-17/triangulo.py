2f703df2105dbb227a500946f7f30a7ec9dd7dac
ericka1599 <erickasegura96@gmail.com>
2017-01-27 08:51:30 -0600

    cambios

---
+++ 27-01-17/triangulo.py
@@ -0,0 +1,16 @@
+ar = open("triangle.txt" , "rb")
+triangulo = []
+for i in ar.readlines():
+	li = [ ]
+	sp = i.split() 
+	for i in sp:
+		li.append(int(i))
+		triangulo.append(li)
+		lar = len(li)
+		c = 0
+		for j in li:
+			if j > c 
+			nva = j
+
+print (triangulo)
+print (lar)
