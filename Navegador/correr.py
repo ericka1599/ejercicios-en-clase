14489749a58743acf956bf2cdbaa6cf3720987a4
ericka1599 <erickasegura96@gmail.com>
2017-02-02 11:47:38 -0600

    dia 2

---
+++ Navegador/correr.py
@@ -0,0 +1,14 @@
+from navegador import Navegador
+from tab import Tabs
+
+print ("Hola!")
+
+if self.abierto == False:
+	desa = int(input("Deseas abrir el navegador?" '\n' "1. Si" '\n' "2. No"))
+	if desa == 1:
+		self.abierto == True
+	else: 
+		print 
+
+else:	
+	deso =int(input("Ahora que quieres hacer?"  '\n' "1. Crear un nuevo tab" '\n' "2. Cambiar la url de un tab" '\n' "3. Cerrar un tab" '\n' "4. Cerrar todos los tab" '\n' "5. Mostrar todos mis tabs" '\n' "6. Guardar mis tabs" '\n' "7.Cerrar el navegador" '\n' "8. Salir"))
