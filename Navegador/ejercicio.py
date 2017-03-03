8ea4a6b1d6e73fe827544b3d01efda7213695434
Doumi <danieldouma@hotmail.es>
2017-03-03 08:48:13 -0600

    d vvc

---
+++ Navegador/ejercicio.py
@@ -0,0 +1,8 @@
+a=(int(input"ingrese su edad"))
+if (a > 17)
+b=int(input("ingrese su nombre:"))
+print (b , "eres mayor de edad:")
+else :
+	b=input ("ingrese el nombre de su casa :")
+		print ("tienes que pagar la casa hoy")
+		
